8cbb66cbb6d466948917046d2cfad8353befc341
izzy-2023 <izzyzinxhirija@gmail.com>
2024-02-05 22:27:56 -0500

    added function that makes the api call to get giphy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Gif from "./components/Gif"
+
+
 
 function App() {
+  const [gifSource, setGifSource] = useState({});
+  
+  const handleSubmit = async () => {
+  const apiKey = 'WN073PPq8eMoNuHwdmHpV5AceZhZHwa7';
+  const gifSource = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
+
+  let res = await fetch(gifSource);
+  let json = await res.json();
+  console.log(json.data.image_url);
+
+  setGifSource(json.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Giphy</h1>
+      <Button handleSubmitApp={handleSubmit}/>
+      {gifSource.image_url ? 
+      <Gif gifSource={gifSource}/> 
+      :
+      <h2>Pull Random Gifs</h2>
+      }
+
     </div>
   );
 }
